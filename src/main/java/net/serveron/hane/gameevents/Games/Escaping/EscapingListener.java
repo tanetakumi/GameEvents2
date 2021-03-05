bfa8001ad289d2197dff01e8b907249e00130c25
tanetakumi <nutantakumi@yahoo.co.jp>
2021-03-05 20:53:31 +0900

    change Escaping class

--- src/main/java/net/serveron/hane/gameevents/Games/Escaping/EscapingListener.java
+++ src/main/java/net/serveron/hane/gameevents/Games/Escaping/EscapingListener.java
@@ -1,22 +1,72 @@
 package net.serveron.hane.gameevents.Games.Escaping;
 
-import net.serveron.hane.gameevents.util.Game;
+import net.kyori.adventure.text.Component;
+import net.serveron.hane.gameevents.GameEvents;
+import net.serveron.hane.gameevents.util.Timer;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scoreboard.Objective;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EscapingListener implements Listener {
+    private final GameEvents plugin;
     private final Escaping game;
-    private Game timer;
+    private Timer timer;
+    private Objective obj;
 
-    public EscapingListener(Escaping game){
+    public List<String> taggerList = new ArrayList<>();
+    public List<String> escapeList = new ArrayList<>();
+
+    public EscapingListener(GameEvents plugin,Escaping game){
+        this.plugin = plugin;
         this.game = game;
-        timer = new Game(game.getGameTime()){
+    }
+
+    public void startGame(){
+        //イベントリスナーの登録
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+        //タイマーの起動
+        timer = new Timer(game.getGameTime()) {
             @Override
-            public void timerFinish(){
-
+            public void finishTimer(){
+                finishGame();
             }
-        }.runTaskTimer(plugin,)
+        };
+        timer.runTaskTimer(plugin,5,20);
+        //
+        for(Player p : Bukkit.getOnlinePlayers()){
+            p.setGameMode(GameMode.ADVENTURE);
+            Team team = plugin.mainScoreboard.getEntryTeam(p.getName());
+            if(team!=null){
+                if(team.getName().equals(tagger)){
+                    taggerList.add(p.getName());
+                    p.teleport(escapingInfo.taggerSpawn);
+                    p.setGameMode(GameMode.ADVENTURE);
+                    p.setWalkSpeed(0.2f);
+                }
+                else if(team.getName().equals(escape)){
+                    p.getInventory().setChestplate(new ItemStack(Material.GOLDEN_CHESTPLATE));
+                    escapeList.add(p.getName());
+                    p.setGameMode(GameMode.ADVENTURE);
+                }
+            }
 
+            p.sendTitle(ChatColor.AQUA+"逃走中", "ゲームスタート",20,20,20);
+        }
+    }
 
+    public void finishGame(){
+
+    }
+
+    public void endGame(){
+        HandlerList.unregisterAll(this);
     }
 
 }
--- src/main/java/net/serveron/hane/gameevents/util/Timer.java
+++ src/main/java/net/serveron/hane/gameevents/util/Timer.java
@@ -1,7 +1,27 @@
 package net.serveron.hane.gameevents.util;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
-interface Timer extends BukkitRunnable {
+public class Timer extends BukkitRunnable {
+    private int time;
 
+    public Timer(int time){
+        this.time = time;
+    }
+
+    @Override
+    public void run(){
+        time--;
+        if(time>0) {
+            Bukkit.getOnlinePlayers().forEach(p -> p.sendActionBar(Component.text("残り時間 "+time/60+"分"+time%60+"秒")));
+        }
+        else {
+            this.cancel();
+            finishTimer();
+        }
+    }
+
+    public void finishTimer(){};
 }
